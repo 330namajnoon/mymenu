12bf6210f93897b48423c3de5fa05a37d00829b7
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2023-01-18 02:04:06 +0100

    Add files via upload

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,15 @@
 import "../styles/app.css";
-
+import { useRef,useEffect } from "react";
+import Menu from "./Menu";
 function App() {
+   
     return ( 
-        <div className="paszamine">
-
+        <div  className="paszamine">
+            <Menu/>
         </div>
      );
+    
+    
 }
 
 export default App;
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import { useRef,useEffect } from "react";
+
+
+function Menu() {
+    return ( 
+        <div style={{top:window.innerHeight +"px"}} className="menu-paszamine">
+
+        </div>
+     );
+}
+
+export default Menu;
